751d8258c1f7c765dd8cd8ee2e0d8afe3157e7e6
Sreejith Sathyadevan <87577589+srij99@users.noreply.github.com>
2021-12-15 19:11:29 +0530

    Update 2_Identity.py

--- Practicals/2_Identity.py
+++ Practicals/2_Identity.py
@@ -3,9 +3,10 @@ def Identity(s1,s2):
     Gap(s1,s2)
     print(s1,s2)
     total_Elements = len(s1)*len(s2)
-    for i in range(len(s1)):
-        if s1[i]==s2[i]:
-            score+=1
+    for i in range(0,len(s1)): 
+        for j in range(0,len(s1)):
+             if s1[i]==s2[j]:
+                score+=1
     identity = (score/total_Elements)*100
     print("The Matching score is ",score)
     print("The Identity is ",identity)
